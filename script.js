e7e4b9d48af4e508c4d1fa3a6342b795dab0bb0d
Cort <91863352+ccdalton17@users.noreply.github.com>
2023-04-06 14:01:07 +0800

    made toggle responsive

--- script.js
+++ script.js
@@ -1,9 +1,5 @@
 const toggleSwitch = document.querySelector('#toggle-switch');
 toggleSwitch.addEventListener('click', function() {
   document.body.classList.toggle('night-mode');
-  //const darkElements = document.querySelectorAll('.dark');
-  //for(let i=0; i<darkElements.length; i++){
-  //  darkElements[i].classList.toggle('night-mode');
-  //}
 });
 
